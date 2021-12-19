8558b9798efdfce422cfe4d8910f2d213bcf900b
chenspa <55611748+chenspa@users.noreply.github.com>
2021-12-19 14:55:47 +0800

    pull commit

--- src/main/java/git_test.java
+++ src/main/java/git_test.java
@@ -7,5 +7,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("git test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
